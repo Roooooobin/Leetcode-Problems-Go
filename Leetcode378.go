4c38bdcb6edf7f825e06643f58302a8b75399cbc
Roooooobin <35558127+Roooooobin@users.noreply.github.com>
2020-07-02 20:21:07 +0800

    add leetcode-378

---
+++ Leetcode378.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func kthSmallest(matrix [][]int, k int) int {
+	var n, m = len(matrix), len(matrix[0])
+	var l, r = matrix[0][0], matrix[n-1][m-1]
+	var mid int
+	for l < r {
+		mid = (l + r) >> 1
+		var cnt int
+		var j = n - 1
+		for i := 0; i < n; i++ {
+			for j >= 0 && matrix[i][j] > mid {
+				j--
+			}
+			cnt += j + 1
+		}
+		if cnt >= k {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	return l
+}
+
+func main() {
+	var mat = [][]int{{1, 2}, {3, 4}}
+	fmt.Println(kthSmallest(mat, 2))
+}
