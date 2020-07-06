55fbf65927972534ffcb2bf26c34447aabb6fb5b
Roooooobin <35558127+Roooooobin@users.noreply.github.com>
2020-07-06 15:45:36 +0800

    add leetcode-63

---
+++ Leetcode63.go
@@ -0,0 +1,23 @@
+package main
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	if obstacleGrid[0][0] == 1 {
+		return 0
+	}
+	m := len(obstacleGrid)
+	n := len(obstacleGrid[0])
+	dp := make([]int, n)
+	dp[0] = 1
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if obstacleGrid[i][j] == 1 {
+				dp[j] = 0
+				continue
+			}
+			if j > 0 {
+				dp[j] += dp[j-1]
+			}
+		}
+	}
+	return dp[n-1]
+}
