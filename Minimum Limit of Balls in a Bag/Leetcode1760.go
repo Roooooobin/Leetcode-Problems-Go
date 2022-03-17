d8d5a30f2540c78061c51defc84bf4108e64e2f7
Roooooobin <lz2294@nyu.edu>
2022-03-17 10:12:47 -0400

    add leetcode-1760

---
+++ Minimum Limit of Balls in a Bag/Leetcode1760.go
@@ -0,0 +1,22 @@
+package main
+
+func minimumSize(nums []int, maxOperations int) int {
+
+	lo, hi := 1, 1000000001
+	for lo <= hi {
+		mid := lo + (hi-lo)>>1
+		op := 0
+		for _, num := range nums {
+			op += (num - 1) / mid
+			if op > maxOperations {
+				break
+			}
+		}
+		if op > maxOperations {
+			lo = mid + 1
+		} else {
+			hi = mid - 1
+		}
+	}
+	return lo
+}
