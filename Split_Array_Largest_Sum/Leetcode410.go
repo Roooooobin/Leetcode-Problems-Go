84d0cf22cad9bc773b19e19c5c00c78c00681ec5
Roooooobin <lz2294@nyu.edu>
2022-03-30 20:51:44 -0400

    add leetcode-410

---
+++ Split_Array_Largest_Sum/Leetcode410.go
@@ -0,0 +1,50 @@
+package main
+
+func splitArray(nums []int, m int) int {
+
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	if m == 1 {
+		return sum
+	}
+	n := len(nums)
+
+	helper := func(g int) bool {
+		cur := 0
+		cnt := 0
+		i := 0
+		for ; i < n; i++ {
+			if nums[i] > g {
+				return false
+			}
+			if cur+nums[i] > g {
+				cnt++
+				cur = 0
+				if cnt == m-1 {
+					break
+				}
+			}
+			cur += nums[i]
+		}
+		cur = 0
+		for ; i < n; i++ {
+			cur += nums[i]
+		}
+		if cur <= g {
+			return true
+		}
+		return false
+	}
+	lo, hi := 0, sum
+	for lo <= hi {
+		mid := lo + (hi-lo)>>1
+		if helper(mid) {
+			hi = mid - 1
+		} else {
+			lo = mid + 1
+		}
+	}
+	return lo
+}
