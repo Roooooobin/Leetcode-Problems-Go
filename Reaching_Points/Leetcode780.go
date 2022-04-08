e6b00bde3858a8f9ab407c09eb1f8c6aef48a4da
Roooooobin <lz2294@nyu.edu>
2022-04-08 16:29:49 -0400

    add leetcode-780

---
+++ Reaching_Points/Leetcode780.go
@@ -0,0 +1,21 @@
+package main
+
+//https://leetcode-cn.com/problems/reaching-points/solution/dao-da-zhong-dian-by-leetcode-solution-77fo/
+func reachingPoints(sx int, sy int, tx int, ty int) bool {
+
+	for tx > sx && ty > sy && tx != ty {
+		if tx > ty {
+			tx %= ty
+		} else {
+			ty %= tx
+		}
+	}
+	if tx == sx && ty == sy {
+		return true
+	} else if tx == sx {
+		return ty > sy && (ty-sy)%tx == 0
+	} else if ty == sy {
+		return tx > sx && (tx-sx)%ty == 0
+	}
+	return false
+}
