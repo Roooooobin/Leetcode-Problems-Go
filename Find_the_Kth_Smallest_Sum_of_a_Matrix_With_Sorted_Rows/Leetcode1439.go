0563244f1d047226fe8fe709a52b6597ef227dd0
Roooooobin <lz2294@nyu.edu>
2022-05-24 08:30:06 -0400

    add leetcode-1439

---
+++ Find_the_Kth_Smallest_Sum_of_a_Matrix_With_Sorted_Rows/Leetcode1439.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"container/heap"
+	"strconv"
+	"strings"
+)
+
+/*
+https://leetcode.cn/problems/find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows/solution/go-xiao-ding-dui-shuang-bai-by-flare-dlcmc/
+*/
+func kthSmallest(a [][]int, K int) int {
+
+	m, n := len(a), len(a[0])
+	h := &Nodes{}
+	heap.Init(h)
+	curSum := 0
+	for i := 0; i < m; i++ {
+		curSum += a[i][0]
+	}
+	heap.Push(h, Node{indexes: make([]int, m), sum: curSum})
+	seen := make(map[string]struct{})
+	for k := 1; k < K; k++ {
+		top := heap.Pop(h).(Node)
+		curSum = top.sum
+		for i, j := range top.indexes {
+			if j < n-1 {
+				newIndexes := make([]int, len(top.indexes))
+				copy(newIndexes, top.indexes)
+				// forward
+				newIndexes[i]++
+				key := makeKey(newIndexes)
+				if _, ok := seen[key]; !ok {
+					newSum := curSum + a[i][j+1] - a[i][j]
+					heap.Push(h, Node{newIndexes, newSum})
+					seen[key] = struct{}{}
+				}
+			}
+		}
+	}
+	return heap.Pop(h).(Node).sum
+}
+
+func makeKey(arr []int) string {
+	var builder strings.Builder
+	for _, x := range arr {
+		builder.WriteString(strconv.Itoa(x))
+	}
+	return builder.String()
+}
+
+type Node struct {
+	indexes []int
+	sum     int
+}
+
+type Nodes []Node
+
+func (h Nodes) Len() int            { return len(h) }
+func (h Nodes) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h Nodes) Less(i, j int) bool  { return h[i].sum < h[j].sum }
+func (h *Nodes) Push(x interface{}) { *h = append(*h, x.(Node)) }
+func (h *Nodes) Pop() interface{} {
+	x := (*h)[h.Len()-1]
+	*h = (*h)[:h.Len()-1]
+	return x
+}
