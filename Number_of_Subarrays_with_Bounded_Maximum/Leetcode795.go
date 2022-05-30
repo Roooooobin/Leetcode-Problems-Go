822da37c2b1e39eebfa8cd3ebb96f7960e369f7d
Roooooobin <lz2294@nyu.edu>
2022-05-29 21:59:54 -0400

    add leetcode-795
    Signed-off-by: Roooooobin <lz2294@nyu.edu>

---
+++ Number_of_Subarrays_with_Bounded_Maximum/Leetcode795.go
@@ -0,0 +1,43 @@
+package main
+
+func numSubarrayBoundedMax(nums []int, left int, right int) int {
+
+	inIdx, largerIdx := -1, -1
+	res := 0
+	for i, num := range nums {
+		if num >= left && num <= right {
+			inIdx = i
+			res += i - largerIdx
+		} else if num < left {
+			res += max(0, inIdx-largerIdx)
+		} else {
+			largerIdx = i
+		}
+	}
+	return res
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func numSubarrayBoundedMax2(nums []int, left int, right int) int {
+
+	count := func(a []int, target int) int {
+		res, cur := 0, 0
+		for _, x := range a {
+			if x <= target {
+				cur++
+			} else {
+				cur = 0
+			}
+			res += cur
+		}
+		return res
+	}
+	return count(nums, right) - count(nums, left-1)
+}
