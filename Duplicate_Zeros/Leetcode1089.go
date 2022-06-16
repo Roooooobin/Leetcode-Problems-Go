3d22beda211ef420e967b8369f54b7abe56360ca
Roooooobin <lz2294@nyu.edu>
2022-06-16 11:11:01 -0400

    add leetcode-1089, 1901

---
+++ Duplicate_Zeros/Leetcode1089.go
@@ -0,0 +1,45 @@
+package main
+
+// space: O(n)
+func duplicateZerosSpaceN(arr []int) {
+
+	n := len(arr)
+	a := make([]int, 0)
+	for i := range arr {
+		if arr[i] == 0 {
+			a = append(a, 0)
+			a = append(a, 0)
+		} else {
+			a = append(a, arr[i])
+		}
+		if len(a) >= n {
+			break
+		}
+	}
+	for i := range arr {
+		arr[i] = a[i]
+	}
+}
+
+// space: O(1)
+func duplicateZeros(arr []int) {
+
+	n := len(arr)
+	count := 0
+	for i := range arr {
+		if arr[i] == 0 {
+			count++
+		}
+	}
+	for i := n - 1; i >= 0; i-- {
+		if arr[i] == 0 {
+			count--
+		}
+		if i+count < n {
+			arr[i+count] = arr[i]
+		}
+		if arr[i] == 0 && i+count+1 < n {
+			arr[i+count+1] = 0
+		}
+	}
+}
---
+++ Find_a_Peak_Element_II/Leetcode1901.go
@@ -0,0 +1,45 @@
+package main
+
+func findPeakGrid(mat [][]int) []int {
+
+	n := len(mat[0])
+	lo, hi := 0, n-1
+	for lo <= hi {
+		mi := lo + (hi-lo)>>1
+		row := getColMax(mat, mi)
+		if mi == 0 {
+			if mat[row][mi] > mat[row][mi+1] {
+				return []int{row, mi}
+			} else {
+				lo = mi + 1
+			}
+		} else if mi == n-1 {
+			if mat[row][mi] > mat[row][mi-1] {
+				return []int{row, mi}
+			} else {
+				hi = mi - 1
+			}
+		} else {
+			if mat[row][mi] > mat[row][mi+1] && mat[row][mi] > mat[row][mi-1] {
+				return []int{row, mi}
+			} else if mat[row][mi] < mat[row][mi+1] {
+				lo = mi + 1
+			} else {
+				hi = mi - 1
+			}
+		}
+	}
+	return []int{}
+}
+
+func getColMax(nums [][]int, col int) int {
+	res := 0
+	resRow := 0
+	for i, a := range nums {
+		if res < a[col] {
+			res = a[col]
+			resRow = i
+		}
+	}
+	return resRow
+}
