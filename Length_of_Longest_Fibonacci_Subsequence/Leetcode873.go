fc3809d71c93e94b1de84dec1419475467ef7b9c
Roooooobin <lz2294@nyu.edu>
2022-06-29 09:30:04 -0400

    add leetcode-132, 873, 1143

---
+++ Length_of_Longest_Fibonacci_Subsequence/Leetcode873.go
@@ -0,0 +1,31 @@
+package main
+
+func lenLongestFibSubseq(arr []int) int {
+
+	n := len(arr)
+	dp := make(map[int]int)
+	mp := make(map[int]int)
+	for i, x := range arr {
+		mp[x] = i
+	}
+	res := 0
+	for i := 1; i < n; i++ {
+		for j := 0; j < i; j++ {
+			if arr[i]-arr[j] >= arr[j] {
+				continue
+			}
+			if idx, ok := mp[arr[i]-arr[j]]; ok {
+				dp[j*n+i] = max(dp[j*n+i], dp[idx*n+j]+1)
+				res = max(dp[j*n+i]+2, res)
+			}
+		}
+	}
+	return res
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
---
+++ Palindrome_PartitioningII/Leetcode132.go
@@ -0,0 +1,45 @@
+package main
+
+func minCut(s string) int {
+
+	n := len(s)
+	isPal := make([][]bool, n)
+	for i := range isPal {
+		isPal[i] = make([]bool, n)
+	}
+	for i := 0; i < n; i++ {
+		isPal[i][i] = true
+	}
+	for i := n - 2; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			if j == i+1 {
+				isPal[i][j] = s[i] == s[j]
+			} else {
+				isPal[i][j] = s[i] == s[j] && isPal[i+1][j-1]
+			}
+		}
+	}
+	dp := make([]int, n)
+	for i := range dp {
+		dp[i] = 0x3f3f3f3f
+	}
+	for i := 0; i < n; i++ {
+		if isPal[0][i] {
+			dp[i] = 0
+		} else {
+			for j := 0; j < i; j++ {
+				if isPal[j+1][i] {
+					dp[i] = min(dp[i], dp[j]+1)
+				}
+			}
+		}
+	}
+	return dp[n-1]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
