5c25f57a37edbb3e0b7abcd53bb8d53fc32bf1f8
Roooooobin <lz2294@nyu.edu>
2022-08-29 15:42:19 -0400

    add leetcode-998

---
+++ Maximum_Binary_Tree_II/Leetcode998.go
@@ -0,0 +1,16 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func insertIntoMaxTree(root *TreeNode, val int) *TreeNode {
+
+	if root == nil || root.Val < val {
+		return &TreeNode{val, root, nil}
+	}
+	root.Right = insertIntoMaxTree(root.Right, val)
+	return root
+}
