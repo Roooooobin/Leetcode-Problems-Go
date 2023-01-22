23e3b506e5038fb1adb09d4869a47de2d07a1082
Roooooobin <lz2294@nyu.edu>
2023-01-22 21:06:49 +0800

    add leetcode-2042

---
+++ Check_if_Numbers_Are_Ascending_in_a_Sentence/Leetcode2042.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func areNumbersAscending(s string) bool {
+
+	words := strings.Split(s, " ")
+	prev := -1
+	for _, word := range words {
+		curr, err := strconv.Atoi(word)
+		if err != nil {
+			continue
+		}
+		if curr <= prev {
+			return false
+		}
+		prev = curr
+	}
+	return true
+}
