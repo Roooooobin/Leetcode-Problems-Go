0a6c788c8258c27fa88c2143870bcb8d5ff94f3e
Roooooobin <lz2294@nyu.edu>
2023-02-01 19:17:16 +0800

    add leetcode-1071

---
+++ Greatest_Common_Divisor_of_Strings/Leetcode1071.go
@@ -0,0 +1,16 @@
+package main
+
+func gcdOfStrings(str1 string, str2 string) string {
+
+	if str1+str2 != str2+str1 {
+		return ""
+	}
+	return str1[:gcd(len(str1), len(str2))]
+}
+
+func gcd(x, y int) int {
+	if y == 0 {
+		return x
+	}
+	return gcd(y, x%y)
+}
